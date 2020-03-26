c279cb37214f1bcab97d4dc40ecc5706a1eb56bd
James Chen <walee_chen@163.com>
2020-03-26 23:56:05 +0800

    add headers

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,17 @@
         >
         </el-option>
       </el-select>
-      <el-input v-model="input" placeholder="请输入URL"></el-input>
-      <el-button type="primary">send</el-button>
+      <el-input v-model="url" placeholder="请输入URL"></el-input>
+      <el-button type="primary" @click="handleSubmit">send</el-button>
     </div>
-    <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tabs v-model="activeName">
       <el-tab-pane label="Params" name="get">
-        <p>{{ items }}</p>
-        <table cellpadding="0" cellspacing="0" ref="table">
+        <table cellpadding="0" cellspacing="0">
           <tr>
             <td>KEY</td>
             <td>VALUE</td>
           </tr>
+
           <tr v-for="(item, index) in items" :key="item.id">
             <td>
               <i class="el-icon-plus" @click="add"></i>
@@ -33,7 +33,42 @@
           </tr>
         </table>
       </el-tab-pane>
-      <el-tab-pane label="Body" name="post">Body</el-tab-pane>
+      <el-tab-pane label="Body" name="post">
+        <table cellpadding="0" cellspacing="0">
+          <tr>
+            <td>KEY</td>
+            <td>VALUE</td>
+          </tr>
+          <tr v-for="(item, index) in formData" :key="item.id">
+            <td>
+              <i class="el-icon-plus" @click="add1"></i>
+              <input v-model="item.key" />
+            </td>
+            <td>
+              <input v-model="item.value" />
+              <i class="el-icon-close" @click="remove1(index)"></i>
+            </td>
+          </tr>
+        </table>
+      </el-tab-pane>
+      <el-tab-pane label="headers" name="headers">
+        <table cellpadding="0" cellspacing="0">
+          <tr>
+            <td>KEY</td>
+            <td>VALUE</td>
+          </tr>
+          <tr v-for="(item, index) in formData" :key="item.id">
+            <td>
+              <i class="el-icon-plus" @click="add2"></i>
+              <input v-model="item.key" />
+            </td>
+            <td>
+              <input v-model="item.value" />
+              <i class="el-icon-close" @click="remove3(index)"></i>
+            </td>
+          </tr>
+        </table>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -43,34 +78,80 @@ export default {
     return {
       options: [
         {
-          value: "0",
+          value: "get",
           label: "get"
         },
         {
-          value: "1",
+          value: "post",
           label: "post"
         }
       ],
       value: "",
-      input: "",
+      url: "",
       activeName: "get",
-      items: [{ id: 1, key: "", value: "" }]
+      items: [{ id: 1, key: "", value: "" }],
+      formData: [{ id: 1, key: "", value: "" }],
+      data: {},
+      headers: [{ id: 1, key: "", value: "" }],
+      header: {}
     };
   },
-  mounted() {
-    this.axios({});
-  },
   methods: {
-    handleClick(tab, event) {
-      console.log(tab, event);
-    },
     add() {
       const length = this.items.length - 1;
       let _id = this.items[length].id + 1;
       this.items.push({ id: _id, key: "", value: "" });
     },
+    add1() {
+      const length = this.formData.length - 1;
+      let _id = this.formData[length].id + 1;
+      this.formData.push({ id: _id, key: "", value: "" });
+    },
+    add2() {
+      const length = this.headers.length - 1;
+      let _id = this.headers[length].id + 1;
+      this.headers.push({ id: _id, key: "", value: "" });
+    },
     remove(index) {
-      this.items.splice(index, 1);
+      const length = this.items.length;
+      if (length > 1) {
+        this.items.splice(index, 1);
+      }
+    },
+    remove1(index) {
+      const length = this.formData.length;
+      if (length > 1) {
+        this.formData.splice(index, 1);
+      }
+    },
+    remove2(index) {
+      const length = this.headers.length;
+      if (length > 1) {
+        this.headers.splice(index, 1);
+      }
+    },
+    handleSubmit() {
+      let URL = this.url;
+      this.items.forEach((value, index) => {
+        if (index === 0) {
+          if (value.key !== "" && value.value !== "")
+            URL += "?" + value.key + "=" + value.value;
+        } else {
+          URL += "&" + value.key + "=" + value.value;
+        }
+      });
+      this.formData.forEach(value => {
+        this.data[value.key] = value.value;
+      });
+      this.headers.forEach(value => {
+        this.header[value.key] = value.value;
+      });
+      this.$axios({
+        method: this.value,
+        url: URL,
+        data: this.data,
+        headers: this.header
+      });
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
-Vue.prototype.axios = axios;
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
